3c7e6e7fee41dfd5b1e1afa0e80f7066b1772c94
Rafat Rahman Tushar <rafat.tushar@northsouth.edu>
2019-08-11 16:12:50 +0600

    Adding password recovery screen
    1. Adding the activity
    2. Adding the appropriate contexts

---
+++ Project/app/src/main/java/edu/northsouth/application/PasswordRecovery.java
@@ -0,0 +1,14 @@
+package edu.northsouth.application;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PasswordRecovery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_recovery);
+    }
+}
